cfe7aecc0490ffaddda1e987b267d67da3c9d31a
Andreina Nathaly Rodriguez Martinez <andreina3nathaly5@gmail.com>
2022-04-08 16:39:04 -0400

    continue editing about page

---
+++ js/about.js
@@ -0,0 +1,27 @@
+window.sr = ScrollReveal();
+
+sr.reveal('.main--pic, .skills', {
+    duration: 3000,
+    origin: 'bottom',
+    reset: true,
+    delay: 50
+        /* distance: '-100px' */
+});
+
+/* animación con origen desde la derecha y delay */
+sr.reveal('.main--title, .skills-title', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-200px',
+    delay: 100,
+    reset: true
+});
+/* animación desde abajo y delay */
+sr.reveal('.main--description, skills-description', {
+    duration: 3000,
+    origin: 'right',
+    distance: '-200px',
+    delay: 200,
+    reset: true
+});
+/* animación top sin delay */
